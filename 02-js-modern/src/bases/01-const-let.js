a6393a7922417412cbf404d57cd76df433c16edb
erikth97 <erik.tamayohe@uanl.edu.mx>
2023-11-14 18:25:24 -0600

    Const and Let

---
+++ 02-js-modern/src/bases/01-const-let.js
@@ -0,0 +1,17 @@
+
+// Variables y Constantes
+
+const nombre = 'Erik'; 
+let apellido = 'Tamayo';
+
+let valorDado = 3;
+valorDado = 4;
+
+console.log(nombre, apellido, valorDado)
+
+if (true) {
+    const nombre = 'Fabian';
+    console.log(nombre)
+}
+
+console.log(valorDado);
