ba1c6cae8d54a0b2b398cf8a170f6a4b9c41fd66
erikth97 <erik.tamayohe@uanl.edu.mx>
2023-11-14 19:11:18 -0600

    Arrays and Call backs

---
+++ 02-js-modern/src/bases/04-arrays.js
@@ -0,0 +1,13 @@
+
+
+const array = [1,2,3,4];
+
+let array2 = [...array, 5];
+
+const array3 = array2.map(function(numero) {
+    return numero * 2;
+})
+
+console.log(array);
+console.log(array2);
+console.log(array3);
