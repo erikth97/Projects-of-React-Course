4673ee384a7065fd6b3ea25ea37a646e3f5d930a
erikth97 <erik.tamayohe@uanl.edu.mx>
2023-11-16 18:09:24 -0600

    destructured arrays

---
+++ 02-js-modern/src/bases/07.destructured-arrays.js
@@ -0,0 +1,23 @@
+
+
+const personajes = ['Goku', 'Vegueta', 'Trunks'];
+const [, , p3] = personajes;
+console.log(p3);
+
+
+const retornaArreglo = () => {
+    return ['ABC', 123];
+}
+
+const [letras, numeros] = retornaArreglo();
+console.log(letras, numeros);
+
+const useState = (valor) => {
+    return [valor, ()=>{console.log('Hola')}];
+}
+
+const [nombre, setNombre] = useState('Goku');
+
+console.log(nombre);
+setNombre();
+
