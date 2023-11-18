dcc7784ccd4a1f4e72bef225577037bbf85549d1
erikth97 <erik.tamayohe@uanl.edu.mx>
2023-11-17 19:32:57 -0600

    First Component

--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {FirstApp} from './FirstApp'
 
-function App() {
-    // docuemnte.createElement...
-    return (<h1>Hola Mundo</h1>);
-}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <App></App>
+        <FirstApp></FirstApp>
     </React.StrictMode>
 )
