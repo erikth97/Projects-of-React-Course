65ce27b3c77a9c01cb6cc5fca673589e77b283ba
erikth97 <erik.tamayohe@uanl.edu.mx>
2023-11-21 20:05:40 -0600

    styles

--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {FirstApp} from './FirstApp'
 import {HellowWorld} from './HelloWorldApp'
+import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
