b90cd91237c4b151c1d1b4743cc035aa38f7bf26
erikth97 <erik.tamayohe@uanl.edu.mx>
2023-11-21 21:03:25 -0600

    Counter App, practice of prop types

--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {FirstApp} from './FirstApp'
 // import {HellowWorld} from './HelloWorldApp'
+import {CounterApp} from './CounterApp';
 import './styles.css';
 
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <FirstApp title="hola, soy Erik" subTitle={123}></FirstApp>
+        {/* <FirstApp title="hola, soy Erik" subTitle={123}></FirstApp> */}
         {/* <HellowWorld></HellowWorld> */}
+        <CounterApp value={10}></CounterApp>
     </React.StrictMode>
 );
