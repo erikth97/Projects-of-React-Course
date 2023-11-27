0fefd2f513fe8c8778d24560e26f80d688107dd1
erikth97 <erik.tamayohe@uanl.edu.mx>
2023-11-27 16:34:49 -0600

    Events click

--- 03-counter-app-vite/src/CounterApp.jsx
+++ 03-counter-app-vite/src/CounterApp.jsx
@@ -1,10 +1,22 @@
 import PropTypes from 'prop-types';
 
 export const CounterApp = ({value}) => {
+
+    const handleAdd = () => {
+        //
+        console.log('+1');
+        value = 1000;
+        console.log(value);
+    }
+
     return (
         <>
             <h1>CounterApp</h1>
-            <h2>{value}</h2>
+            <h2> { value } </h2>
+
+            <button>
+                +1
+            </button>
         </>
     )
 }
