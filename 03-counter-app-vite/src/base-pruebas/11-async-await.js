3d97dbf122985f76c9541e5fd2acf5fc20bb9dcf
erikth97 <erik.tamayohe@uanl.edu.mx>
2023-11-29 17:27:08 -0600

    Unit Test with async-await

--- 03-counter-app-vite/src/base-pruebas/11-async-await.js
+++ 03-counter-app-vite/src/base-pruebas/11-async-await.js
@@ -1,25 +1,20 @@
 
-
-// const getImagenPromesa = () => new Promise( resolve => resolve('https://ajskdhaskjdhajs.com') )
-// getImagenPromesa().then( console.log );
-
-const getImagen = async() => {
+export const getImagen = async() => {
 
     try {
 
-        const apiKey = 'C1khQe3Z7R1W2lfTO9myKeuShdqFYSGC';
+        const apiKey = '5HhX7ACoqyWtDEDaPmKcn9siwxIM3Zhs';
         const resp   = await fetch(`http://api.giphy.com/v1/gifs/random?api_key=${ apiKey }`);
         const { data } = await resp.json(); 
 
         const { url } = data.images.original;
 
-        const img = document.createElement('img');
-        img.src = url;
-        document.body.append( img );
+        return url;
 
     } catch (error) {
         // manejo del error
-        console.error(error)
+        // console.error(error)
+        return 'No se encontro la imagen'
     }
     
     
@@ -27,6 +22,5 @@ const getImagen = async() => {
 }
 
  getImagen();
-
 
 
