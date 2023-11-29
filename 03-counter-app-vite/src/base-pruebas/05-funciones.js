de048597bd5b700db4eb75ef4b1337bfb5ac6ff4
erikth97 <erik.tamayohe@uanl.edu.mx>
2023-11-29 13:50:22 -0600

    ToEqual

--- 03-counter-app-vite/src/base-pruebas/05-funciones.js
+++ 03-counter-app-vite/src/base-pruebas/05-funciones.js
@@ -1,13 +1,13 @@
 
 
-const getUser = () => ({
+export const getUser = () => ({
         uid: 'ABC123',
         username: 'El_Papi1502'
 });
 
 
 
-const getUsuarioActivo = ( nombre ) =>({
+export const getUsuarioActivo = ( nombre ) =>({
     uid: 'ABC567',
     username: nombre
 })
