047ee211ea948de9b6ee7b56a7f51433e9f25f9f
erikth97 <erik.tamayohe@uanl.edu.mx>
2023-11-29 21:01:30 -0600

    Events Simulation

--- 03-counter-app-vite/src/CounterApp.jsx
+++ 03-counter-app-vite/src/CounterApp.jsx
@@ -1,13 +1,13 @@
-import {useState} from 'react'
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 
 export const CounterApp = ({ value }) => {
+    
+    const [ counter, setCounter ] = useState( value );
 
-    const [ counter, setCounter ] =  useState( value );
-
-    const handleAdd = () => {
+    const handleAdd = () => { 
         // console.log(event)
-        setCounter(counter + 1);
+        setCounter( counter + 1 );
         // setCounter( (c) => c + 1 )
     }
 
@@ -19,9 +19,9 @@ export const CounterApp = ({ value }) => {
             <h1>CounterApp</h1>
             <h2> { counter } </h2>
 
-            <button onClick={ handleAdd }> 1 </button>
+            <button onClick={ handleAdd }> +1 </button>
             <button onClick={ handleSubstract }> -1 </button>
-            <button onClick={ handleReset }> Reset </button>
+            <button aria-label="btn-reset" onClick={ handleReset }> Reset </button>
         </>
     );
 }
@@ -29,3 +29,4 @@ export const CounterApp = ({ value }) => {
 CounterApp.propTypes = {
     value: PropTypes.number.isRequired
 }
+
--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {CounterApp} from './CounterApp';
+import { CounterApp } from './CounterApp';
 
-import {FirstApp} from './FirstApp'
-// import {HellowWorld} from './HelloWorldApp'
+import { FirstApp } from './FirstApp';
+// import { HelloWorldApp } from './HelloWorldApp';
 import './styles.css';
 
 
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot( document.getElementById('root') ).render(
     <React.StrictMode>
-        {/* <CounterApp value={10}></CounterApp> */}
-        <FirstApp title="hola, soy Erik"></FirstApp>
+        <CounterApp value={ 20 } />
+        {/* <FirstApp title="Hola, Soy Vegeta" /> */}
     </React.StrictMode>
 );
+
