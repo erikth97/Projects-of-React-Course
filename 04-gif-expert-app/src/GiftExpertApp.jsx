bea55db904dffa2dac7953b3a92bb1aa9fe307c1
erikth97 <erik.tamayohe@uanl.edu.mx>
2023-12-04 13:29:04 -0600

    App

--- 04-gif-expert-app/src/GiftExpertApp.jsx
+++ 04-gif-expert-app/src/GiftExpertApp.jsx
@@ -1,8 +1,27 @@
+import { useState } from "react";
 
 export const GiftExpertApp = () => {
-  return (
-    <>
+
+    const [ categories, setCategories ] = useState([ 'One Punch', 'Dragon Ball' ]);
+
+    console.log(categories);
+
+    return (
+        <>
+        {/* {titulo} */}
         <h1>Gif App</h1>
-    </>
-  )
+
+        {/* {Input}
+
+        {Listado de Gif} */}
+        <ol>
+            {
+                categories.map( category => {
+                    return <li key={ category }>{ category }</li>
+                })
+            }
+        </ol>
+
+        </>
+    )
 }
