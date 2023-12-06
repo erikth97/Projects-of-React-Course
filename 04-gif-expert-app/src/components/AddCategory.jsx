c323e729f2a3aa4ff28b9b5b76b99b5db2bd4a17
erikth97 <erik.tamayohe@uanl.edu.mx>
2023-12-05 23:13:49 -0600

    Comunication btw components

--- 04-gif-expert-app/src/components/AddCategory.jsx
+++ 04-gif-expert-app/src/components/AddCategory.jsx
@@ -1,21 +1,24 @@
 import { useState } from 'react';
 
 
-export const AddCategory = () => {
+export const AddCategory = ({ setCategories }) => {
 
     const [ inputValue, setInputValue ] = useState('One Punch');
 
     const onInputChange = ({ target }) => {
         setInputValue( target.value );
-        }
+    }
 
     const onSubmit = ( event ) => {
         event.preventDefault();
-        console.log(inputValue);
+        if ( inputValue.trim().length <= 1) return;
+        
+        setCategories( categories => [ inputValue, ...categories]);
+        setInputValue('');
     }
 
     return (
-        <form onSubmit={ (event) => onSubmit(event) }>
+        <form onSubmit={ onSubmit }>
             <input
                 type="text"
                 placeholder="Buscar Gif"
