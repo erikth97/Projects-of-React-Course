8d83911bc9f0a93dfee284541bf8d513f9de4882
erikth97 <erik.tamayohe@uanl.edu.mx>
2023-12-11 14:48:24 -0600

    Class name

--- 04-gif-expert-app/src/components/GifGrid.jsx
+++ 04-gif-expert-app/src/components/GifGrid.jsx
@@ -1,4 +1,6 @@
 import { useEffect, useState } from "react";
+
+import { GifItem } from "./GifItem";
 import { getGifs } from "../helpers/getGifs";
 
 
@@ -20,14 +22,17 @@ export const GifGrid = ({ category }) => {
         <>
             <h3>{ category }</h3>
 
-            <ol>
+            <div className="card-grid">
                 {
-                    images.map( ({ id, title }) => (
-                        <li key={ id }>{ title }</li>
+                    images.map( ({ image }) => (
+                        <GifItem 
+                            key={ image.id } 
+                            { ...image }
+                        />
                     ))
                 }
 
-            </ol>
+            </div>
 
         </>
     )
