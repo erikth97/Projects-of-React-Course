f14faa39493efe1bb10a1ce0615cc368f7d982e6
erikth97 <erik.tamayohe@uanl.edu.mx>
2023-12-11 14:22:57 -0600

    Titles on images

--- 04-gif-expert-app/src/components/GifGrid.jsx
+++ 04-gif-expert-app/src/components/GifGrid.jsx
@@ -1,9 +1,15 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { getGifs } from "../helpers/getGifs";
 
 
 export const GifGrid = ({ category }) => {
 
+    const [images, setImages] = useState([]);
+
+    const GetImages = async() => {
+        const newImages = await getGifs( category );
+        setImages(newImages);
+    }
 
     useEffect( () => {
         getGifs( category );
@@ -14,6 +20,14 @@ export const GifGrid = ({ category }) => {
         <>
             <h3>{ category }</h3>
 
+            <ol>
+                {
+                    images.map( ({ id, title }) => (
+                        <li key={ id }>{ title }</li>
+                    ))
+                }
+
+            </ol>
 
         </>
     )
