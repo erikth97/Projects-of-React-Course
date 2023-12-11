5749fd8fb50aeffed7eeebaec55529e87ca2693c
erikth97 <erik.tamayohe@uanl.edu.mx>
2023-12-11 13:42:27 -0600

    Build Prod

--- 04-gif-expert-app/src/components/GifGrid.jsx
+++ 04-gif-expert-app/src/components/GifGrid.jsx
@@ -1,10 +1,9 @@
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import { getGifs } from "../helpers/getGifs";
 
 
 export const GifGrid = ({ category }) => {
 
-    const [counter, setCounter] = useState(10);
 
     useEffect( () => {
         getGifs( category );
@@ -15,17 +14,6 @@ export const GifGrid = ({ category }) => {
         <>
             <h3>{ category }</h3>
 
-            <h5>{ counter }</h5>
-            <button onClick={ () => setCounter(counter +1) } >+1</button>
-            
-        </>
-    )
-
-    getGifs(category);
-
-    return (
-        <>
-            <h3>{category}</h3>
 
         </>
     )
