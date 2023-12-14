f36c2d8e681b73454d7142103b4d7d97fab44f5a
erikth97 <erik.tamayohe@uanl.edu.mx>
2023-12-14 01:21:57 -0600

    eslintrc & babel conf, changed babel "js" to "cjs"

--- 04-gif-expert-app/babel.config.cjs
+++ 04-gif-expert-app/babel.config.cjs
@@ -1,4 +1,4 @@
-module.exports = {
+module.exports ={
     presets: [
         [ '@babel/preset-env', { targets: { esmodules: true } } ],
         [ '@babel/preset-react', { runtime: 'automatic' } ],
