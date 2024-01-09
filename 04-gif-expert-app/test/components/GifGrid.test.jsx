13d1723f5267823304b997cac69e2bf01d83c90e
erikth97 <erik.tamayohe@uanl.edu.mx>
2024-01-08 22:20:15 -0600

    CustomHooks Testing

--- 04-gif-expert-app/test/components/GifGrid.test.jsx
+++ 04-gif-expert-app/test/components/GifGrid.test.jsx
@@ -1,16 +1,51 @@
-import { render, screen } from "@testing-library/react";
-import { GifGrid } from "../../src/components/GifGrid";
+import { render, screen } from '@testing-library/react';
+import { GifGrid } from '../../src/components/GifGrid';
+import { useFetchGifs } from '../../src/hooks/useFetchGifs';
+
+jest.mock('../../src/hooks/useFetchGifs');
 
 
-describe('Pruebas en <GifGrid />', () => { 
-
+describe('Pruebas en <GifGrid />', () => {
+    
     const category = 'One Punch';
 
     test('debe de mostrar el loading inicialmente', () => {
 
-        render( <GifGrid category={category} /> );
+        useFetchGifs.mockReturnValue({
+            images: [],
+            isLoading: true
+        });
+
+
+        render( <GifGrid category={ category } /> );
         expect( screen.getByText( 'Cargando...' ) );
         expect( screen.getByText( category ) );
+
+    });
+
+    test('debe de mostrar items cuando se cargan las imágenes useFetchGifs', () => {
+        
+        const gifs = [
+            {
+                id: 'ABC',
+                title: 'Saitama',
+                url: 'https://localhost/saitama.jpg'
+            },
+            {
+                id: '123',
+                title: 'Goku',
+                url: 'https://localhost/goku.jpg'
+            },
+        ]
+
+        useFetchGifs.mockReturnValue({
+            images: gifs,
+            isLoading: false
+        });
+
+        render( <GifGrid category={ category } /> );
+        expect( screen.getAllByRole('img').length ).toBe(2);
+        
 
 
     });
