e27365770522348a1a36b727363aa117b525b034
erikth97 <erik.tamayohe@uanl.edu.mx>
2024-01-08 21:33:18 -0600

    GifGrid Testing

--- 04-gif-expert-app/test/components/AddCategory.test.jsx
+++ 04-gif-expert-app/test/components/AddCategory.test.jsx
@@ -1,9 +1,9 @@
-import { fireEvent, render, screen } from "@testing-library/react"
-import { AddCategory } from "../../src/components/AddCategory"
+import { fireEvent, render, screen } from '@testing-library/react';
+import { AddCategory } from '../../src/components/AddCategory';
 
 
-describe('Pruebas en <AddCategory />', () => { 
-
+describe('Pruebas en <AddCategory />', () => {
+    
     test('debe de cambiar el valor de la caja de texto', () => {
         
         render( <AddCategory onNewCategory={ () => {} } /> );
@@ -14,24 +14,40 @@ describe('Pruebas en <AddCategory />', () => {
         expect( input.value ).toBe('Saitama');
 
     });
-    
-    test('debe de llamar inNewCategory si el onput tiene un valor', () => {
 
-        const inputValue = 'Saitama';
-        //TODO:
+    test('debe de llamar onNewCategory si el input tiene un valor', () => {
+        
+        const inputValue    = 'Saitama';
+        const onNewCategory = jest.fn();
 
-        render( <AddCategory onNewCategory={ () => {} } /> );
+        render( <AddCategory onNewCategory={ onNewCategory } /> );
 
         const input = screen.getByRole('textbox');
-        const form = screen.getByRole('form');
+        const form  = screen.getByRole('form');
 
-        fireEvent.input( input, { target: { value: inputValue }})
-        fireEvent.submit( form )
-        //screen.debug();
+        fireEvent.input( input, { target: { value: inputValue } });
+        fireEvent.submit( form );
+        // screen.debug();
         expect( input.value ).toBe('');
 
+        expect( onNewCategory ).toHaveBeenCalled();
+        expect( onNewCategory ).toHaveBeenCalledTimes(1);
+        expect( onNewCategory ).toHaveBeenCalledWith( inputValue );
 
+    });
 
-    })
+    test('no debe de llamar el onNewCategory si el input está vació', () => {
+        
+        const onNewCategory = jest.fn();
+        render( <AddCategory onNewCategory={ onNewCategory } /> );
+
+        const form = screen.getByRole('form');
+        fireEvent.submit( form );
+
+        expect( onNewCategory ).toHaveBeenCalledTimes(0);
+        expect( onNewCategory ).not.toHaveBeenCalled();
+
+    });
+
 
 });
