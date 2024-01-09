2643633a74a6c63e14b43c60deb18426268b7caa
erikth97 <erik.tamayohe@uanl.edu.mx>
2024-01-08 21:33:13 -0600

    GifGrid Testing

--- 04-gif-expert-app/src/components/AddCategory.jsx
+++ 04-gif-expert-app/src/components/AddCategory.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
 
 export const AddCategory = ({ onNewCategory }) => {
 
@@ -19,7 +19,7 @@ export const AddCategory = ({ onNewCategory }) => {
     }
 
     return (
-        <form onSubmit={ onSubmit } aria-label="form" >
+        <form onSubmit={ onSubmit } aria-label="form">
             <input 
                 type="text"
                 placeholder="Buscar gifs"
