f8c81b562f35b8d56162b92ce584155861d292aa
erikth97 <erik.tamayohe@uanl.edu.mx>
2024-02-13 21:29:40 -0600

    useFetch custom hook

--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -4,14 +4,15 @@ import ReactDOM from 'react-dom/client'
 // import { HooksApp } from './HooksApp'
 // import { CounterApp } from './01-useState/CounterApp'
 // import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
-import { SimpleForm } from './02-useEffect/SimpleForm'
+// import { SimpleForm } from './02-useEffect/SimpleForm'
+import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook';
+import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks';
 
 
 import './index.css'
-import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <FormWithCustomHook/>
-  </React.StrictMode>,
+  // <React.StrictMode>
+    <MultipleCustomHooks />
+  // </React.StrictMode>
 )
