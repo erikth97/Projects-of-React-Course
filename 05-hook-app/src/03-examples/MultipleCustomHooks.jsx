9b800c77b240569e571d1aacaf068e855d349420
erikth97 <erik.tamayohe@uanl.edu.mx>
2024-02-20 19:00:03 -0600

    Connecting Hooks

--- 05-hook-app/src/03-examples/MultipleCustomHooks.jsx
+++ 05-hook-app/src/03-examples/MultipleCustomHooks.jsx
@@ -1,9 +1,11 @@
+import { useCounter } from "../hooks/useCounter";
 import { useFetch } from "../hooks/useFetch"
 
 
 export const MultipleCustomHooks = () => {
 
-        const { data, isLoading, quote: any } = useFetch('https://api.breakingbadquotes.xyz/v1/quotes/');
+        const { counter, increment } = useCounter(1);
+        const { data, isLoading, hasError } = useFetch(`https://api.breakingbadquotes.xyz/v1/quotes/${ counter }`);
         const { author, quote } = !!data && data[0];
     
     return (
@@ -27,7 +29,10 @@ export const MultipleCustomHooks = () => {
             }
 
 
-            <button className="btn btn-primary">
+            <button 
+                className="btn btn-primary"
+                disabled={ isLoading }
+                onClick={ () => increment() }>
                 Next Quote
             </button>
 
